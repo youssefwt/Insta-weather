4482b6f5e09574185988a235e25a363827cbefa8
youssefw <ywtalaat@gmail.com>
2023-11-27 21:15:51 +0200

    edit height and break point in TodayWeather

--- components/TodayWeather.tsx
+++ components/TodayWeather.tsx
@@ -22,16 +22,16 @@ const TodayWeather = ({ unit, current, dayForecast: forecast, location }: PropTy
     };
     const formattedDate = new Intl.DateTimeFormat('en-US', dateOptions).format(date);
     return (
-        <article className='flex flex-col gap-6 md:flex-row justify-between items-center h-[230px]'>
+        <article className='flex flex-col gap-6 lg:flex-row justify-between items-center lg:h-[210px]'>
             {/* condition */}
-            <div className='flex flex-col justify-between h-full text-center md:text-start'>
+            <div className='flex flex-col justify-between h-full text-center lg:text-start'>
                 <h2 className='font-bold text-5xl'>{location?.name}</h2>
-                <p className='font-semibold text-lg my-3'>{formattedDate}</p>
-                <Image className='w-24' src={current?.condition.icon.replace(/^\/\//, "https://") || ""} alt='condition icon' width={64} height={64} />
+                <p className='font-semibold text-lg '>{formattedDate}</p>
+                <Image className='w-24 mx-auto lg:mx-0' src={current?.condition.icon.replace(/^\/\//, "https://") || ""} alt='condition icon' width={64} height={64} />
                 <p className='font-bold text-2xl'>{current?.condition.text}</p>
             </div>
             {/* weather */}
-            <div className='flex flex-col gap-2 justify-between h-full text-center md:text-end'>
+            <div className='flex flex-col gap-2 justify-between h-full text-center lg:text-end'>
                 <p className='font-bold text-8xl '>{temp && Math.round(temp)}&deg;</p>
                 <p className=' text-4xl '>{maxTemp && Math.round(maxTemp)}&deg;{" "}/{" "} <span className='text-neutral-200'>{minTemp && Math.round(minTemp)}&deg;</span></p>
                 <p className='text-lg font-medium'>{forecast?.condition.text} through the day</p>
